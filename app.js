4b48f8debfc87d25e6fe4c7a60c7e692b9b80170
정우정 <112058491+woojeong0117@users.noreply.github.com>
2022-10-22 00:38:34 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+// 서버 구동하는 자바스크립트 파일
+
+const express = require('express');
+
+const http = require('http');
+
+// express 실행한 내용을 app에 담음
+const app = express();
+
+const path = require('path');
+const server = http.createServer(app);
+const socketIO = require('socket.io');
+const moment = require('moment');
+const io = socketIO(server);
+
+// 보여줄 폴더 지정
+app.use(express.static(path.join(__dirname, 'src')));
+
+const PORT = process.env.PORT || 5000;
+
+io.on('connection', (socket) => {
+  socket.on('chatting', (data) => {
+    const { name, msg } = data;
+    io.emit('chatting', {
+      name,
+      msg,
+      time: moment().format('h:mm A'),
+    });
+  });
+});
+
+server.listen(PORT, () => console.log(`server is running ${PORT}`));
